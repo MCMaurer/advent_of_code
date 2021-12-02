0ecea824688fcddf188c5ccddf77e4b9ba54ce2e
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2021-12-02 12:54:57 -0600

    got day 2 tidy

---
+++ 2021/day_2/day_2.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(slider)
+
+d <- read_table("2021/day_2/input.txt", col_names = c("dir", "len"))
+
+
+# part 1 ------------------------------------------------------------------
+
+d %>% 
+  group_by(dir) %>% 
+  summarise(total_len = sum(len)) %>% 
+  pivot_wider(names_from = dir, values_from = total_len) %>% 
+  mutate(ans = (down - up)*forward)
+
+
+# part 2 loop ------------------------------------------------------------------
+
+aim <- 0
+horiz <- 0
+depth <- 0
+
+for (i in seq_along(d$dir)) {
+  print(i)
+  
+  if (d$dir[i] == "up"){
+    aim <- aim - d$len[i]
+  }
+  
+  if (d$dir[i] == "down"){
+    aim <- aim + d$len[i]
+  }
+  
+  if (d$dir[i] == "forward"){
+    horiz <- horiz + d$len[i]
+    depth <- depth + d$len[i] * aim
+  }
+  
+}
+
+horiz*depth
+
+# part 2 tidy -------------------------------------------------------------
+
+d %>% 
+  mutate(len = if_else(dir == "up", -len, len),
+         aim = if_else(dir == "forward", 0, len),
+         cumu_aim = slide_dbl(aim, sum, .before = Inf)) %>% 
+  filter(dir == "forward") %>% 
+  summarise(ans = sum(len*cumu_aim) * sum(len))
+
+
